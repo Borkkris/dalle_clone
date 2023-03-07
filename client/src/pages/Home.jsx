fe404903f3c1e8ccc3edb9d35e46b6facb42cafa
Borkkris <Borkkris@users.noreply.github.com>
2023-03-07 22:23:52 +0100

    updated into the real deploy urls

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -25,7 +25,7 @@ const Home = () => {
     const fetchPosts = async() => {
       setLoading(true)
       try {
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/post', {
+        const response = await fetch('https://dall-e-97hy.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
